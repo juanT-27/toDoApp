9c52205192afdd1e1b8efbe5362c463e7128d49e
juanT-27 <jtsoftdeveloper@gmail.com>
2023-02-20 10:22:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// const d = document;
+
+// let tasks= []
+// let tasksAdded= d.querySelector(".tasksAdded");
+// let fr= d.createDocumentFragment();
+// let template= d.querySelector(".taskCard").content;
+
+// function addTask(ev){
+//  ev.preventDefault()
+// let $form= d.querySelector("#newTaskForm");
+
+//  let newTask= {
+//     title: d.querySelector("#taskTitle").value,
+//     date: d.querySelector("#taskDate").value,
+//     description: d.querySelector("#taskDscp").value
+//  };
+// !
+//  tasks.push(newTask)
+
+//  tasks.forEach(el => {
+//    template.querySelector(".titleF").textContent= el.title;
+//    template.querySelector(".date").textContent= el.date;
+//    template.querySelector(".des").textContent=el.description;
+//    let clone= document.importNode(template, true);
+//    fr.appendChild(clone)
+    
+//  });
+
+// tasksAdded.appendChild(fr)
+//  console.log(tasks)
+//  $form.reset()
+// }
+
+
+
+// d.addEventListener("DOMContentLoaded", e=>{
+//     d.querySelector("#sendTask").addEventListener("click", addTask)
+//    d.querySelector("#deleteBtn").addEventListener("click", deletes)})
+
+
+const d= document;
+
+d.addEventListener("DOMContentLoaded", e=>{
+  
+})
